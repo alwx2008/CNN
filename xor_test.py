f742718312d3b839821c74dd2b6dd800192f0fb8
werewolf lord <40964777+alwx2008@users.noreply.github.com>
2023-09-23 04:35:32 +0000

    Update

--- xor_test.py
+++ xor_test.py
@@ -1,7 +1,7 @@
+import numpy as np
 from dense import Dense
 from activations import Tanh
 from losses import mse, mse_prime
-import numpy as np
 
 X = np.reshape([[0, 0], [0, 1], [1, 0], [1, 1]], (4, 2, 1))
 Y = np.reshape([[0], [1], [1], [0]], (4, 1, 1))
@@ -23,7 +23,7 @@ for e in range(epochs):
         output = x
         for layer in network:
             output = layer.forward(output)
-        
+
         # error calculation
         error += mse(y, output)
 
@@ -31,6 +31,6 @@ for e in range(epochs):
         grad = mse_prime(y, output)
         for layer in reversed(network):
             grad = layer.backward(grad, learning_rate)
-        
+
         error /= len(X)
         print('%d/%d, error=%f' % (e + 1, epochs, error))
