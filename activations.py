ab59b842b4c5a290af7d926ebc71879cbdb22ab8
werewolf lord <40964777+alwx2008@users.noreply.github.com>
2023-09-23 04:28:29 +0000

    Update and create test

--- activations.py
+++ activations.py
@@ -12,3 +12,9 @@ class Activation(Layer):
     
     def backward(self, output_gradient, learning_rate):
         return np.multiply(output_gradient, self.activation_prime(self.input))
+
+class Tanh(Activation):
+    def __init__(self):
+        tanh = lambda x: np.tanh(x)
+        tanh_prime = lambda x: 1 - np.tanh(x) **2
+        super.__init__(tanh, tanh_prime)
---
+++ xor_test.py
@@ -0,0 +1,36 @@
+from dense import Dense
+from activations import Tanh
+from losses import mse, mse_prime
+import numpy as np
+
+X = np.reshape([[0, 0], [0, 1], [1, 0], [1, 1]], (4, 2, 1))
+Y = np.reshape([[0], [1], [1], [0]], (4, 1, 1))
+
+network = [
+    Dense(2, 3),
+    Tanh(),
+    Dense(3, 1),
+    Tanh()
+]
+
+epochs = 10000
+learning_rate = 0.1
+# training
+for e in range(epochs):
+    error = 0
+    for x, y in zip(X, Y):
+        # forward propagation
+        output = x
+        for layer in network:
+            output = layer.forward(output)
+        
+        # error calculation
+        error += mse(y, output)
+
+        # backward propagation
+        grad = mse_prime(y, output)
+        for layer in reversed(network):
+            grad = layer.backward(grad, learning_rate)
+        
+        error /= len(X)
+        print('%d/%d, error=%f' % (e + 1, epochs, error))
